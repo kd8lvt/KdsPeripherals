e4d5cf5d0558a7c1b1a563845fd25f700b0bd04c
kd8lvt <redunderpant@gmail.com>
2025-01-17 12:24:45 -0500

    Initial commit

---
+++ src/client/java/com/kd8lvt/KdsPeripheralsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kd8lvt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KdsPeripheralsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
